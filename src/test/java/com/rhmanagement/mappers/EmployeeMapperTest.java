b4f5a6e7239a8b73ca36eedfa1d95b207de21c98
GuiLima9 <guilherme_limasilva@hotmail.com>
2025-10-01 18:51:45 -0300

    Subindo teste corrigido

--- src/test/java/com/rhmanagement/mappers/EmployeeMapperTest.java
+++ src/test/java/com/rhmanagement/mappers/EmployeeMapperTest.java
@@ -2,46 +2,42 @@ package com.rhmanagement.mappers;
 
 import com.rhmanagement.domains.Employee;
 import com.rhmanagement.dtos.EmployeeResponseDTO;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.Assert.assertEquals;
 
 class EmployeeMapperTest {
-
-    private Employee existEmployee;
-    private EmployeeMapper employeeMapper;
-
-    @BeforeEach
-    void setUp(){
-        employeeMapper = new EmployeeMapper();
-
-        existEmployee = Employee.builder()
-                .id(1L)
-                .name("Nome1")
-                .cargo("Analista Jr")
-                .salario(2500.00)
-                .statusPagamento(true)
-                .build();
-    }
+    private final EmployeeMapper employeeMapper = new EmployeeMapper();
 
     @Test
     public void convertToEntityTest() {
-        Employee dto = existEmployee;
+        final var existEmployee = Employee.builder()
+                .id(1L)
+                .name("Guilherme")
+                .cargo("Estagiario")
+                .salario(1.00)
+                .statusPagamento(true)
+                .build();
 
-        assertEquals(dto.getName(), existEmployee.getName());
-        assertEquals(dto.getCargo(),existEmployee.getCargo());
-        assertEquals(dto.getSalario(),existEmployee.getSalario());
-        assertEquals(dto.getStatusPagamento(),existEmployee.getStatusPagamento());
+        assertEquals(existEmployee.getName(), existEmployee.getName());
+        assertEquals(existEmployee.getCargo(), existEmployee.getCargo());
+        assertEquals(existEmployee.getSalario(), existEmployee.getSalario());
+        assertEquals(existEmployee.getStatusPagamento(), existEmployee.getStatusPagamento());
     }
 
     @Test
     public void convertToResponseDTOTest() {
-        EmployeeResponseDTO responseDTO = employeeMapper.convertToResponseDTO(existEmployee);
+        final var existEmployee = Employee.builder()
+                .id(1L)
+                .name("Guilherme")
+                .cargo("Estagiario")
+                .salario(1.0)
+                .statusPagamento(true)
+                .build();
+        final EmployeeResponseDTO responseDTO = employeeMapper.convertToResponseDTO(existEmployee);
 
-        assertEquals(existEmployee.getName(),responseDTO.getName());
-        assertEquals(existEmployee.getCargo(),responseDTO.getCargo());
-        assertEquals(existEmployee.getSalario(),responseDTO.getSalario());
-        assertEquals(existEmployee.getStatusPagamento(),responseDTO.getStatusPagamento());
+        assertEquals(existEmployee.getName(), responseDTO.getName());
+        assertEquals(existEmployee.getCargo(), responseDTO.getCargo());
+        assertEquals(existEmployee.getSalario(), responseDTO.getSalario());
+        assertEquals(existEmployee.getStatusPagamento(), responseDTO.getStatusPagamento());
     }
 }
